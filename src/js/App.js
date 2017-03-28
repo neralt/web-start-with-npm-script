2fce395ddaa875e34b3a24f6c89575da1459430d
neralt <neralty@gmail.com>
2017-03-28 20:52:19 +0900

    jsをシンプルにして動いた

--- src/js/App.js
+++ src/js/App.js
@@ -2,11 +2,5 @@ import Util from './Util.js';
 
 // Application entry point
 window.onload = () => {
-  const date = Util.formatDate();
-  console.log( '[' + date + '] Application was launched.' );
-
-  const elm = document.querySelector( '.date' );
-  if( elm ) {
-    elm.textContent = date;
-  }
+  new Util("test name");
 };
